064c77a225e71e7f476dee1653cce95d343ee6a0
Daniel Hones <danielhones@gmail.com>
2015-04-05 22:08:29 -0400

    reorganized a bit, started reimplementing entire thing

--- mathentry.py
+++ mathentry.py
@@ -15,18 +15,8 @@ the node that is currently being edited (contains the cursor) is self.active_nod
 
 TODO:
 * Write unit tests
-* Start thinking about how to translate the tree into a valid Python expression
 * Figure out where error handling is needed and write it (maybe ensure that data is always a list of strings?).
-* Implement delete_node or whatever needs to be done to remove a node in the tree.  Also determine
-  what that even means.  Maybe a delete_subtree is also necessary.  Think about it.  Maybe a delete_subtree method
-  and a delete_node_and_subtree method are needed.  Deleting just a node and not its subtree if it has one is pretty
-  much useless and will just leave unused but still referenced objects floating around.
 """
-
-# The only thing we use from constants now is LATEX_COMMANDS, so there is probably a neater
-# way to do it.
-from constants import *
-import keybindings
 
 
 class Observable(object):
@@ -41,278 +31,134 @@ class Observable(object):
         for callback in self.observers:
             callback(self)
 
-class SiblingTree(object):
-    """
-    A tree structure where each node can have an arbitrary number of children, using
-    a left child, right sibling representation
-    """
-    def __init__(self,
-                 data=None,
-                 parent=None,
-                 left_child=None,
-                 left_sibling=None,
-                 right_sibling=None,
-                 root=None):
-        self.data = data
-        self.parent = parent
-        self.left_child = left_child
-        self.right_sibling = right_sibling
-        self.left_sibling = left_sibling
-        if root == None:
-            self.root = self
-        else:
-            self.root = root
 
-    def insert_child(self, new_data):
-        if self.left_child == None:
-            self.left_child = SiblingTree(new_data, root=root, parent=self)
-        else:
-            new_tree = SiblingTree(new_data,
-                                   left_child=self.left_child,
-                                   root=self.root,
-                                   parent=self)
-            self.left_child.parent = new_tree
-            self.left_child = new_tree
-        return self.left_child
-
-    # maybe this class definition should have an insert_left_sibling method for the sake of completeness,
-    # even though it won't be used by the PrettyMath class 
-    def insert_rightsibling(self, new_data):
-        if self.right_sibling == None:
-            self.right_sibling = SiblingTree(new_data,
-                                             left_sibling=self,
-                                             parent=self.parent,
-                                             root=self.root)
-        else:
-            new_tree = SiblingTree(new_data,
-                                   right_sibling=self.right_sibling,
-                                   left_sibling=self,
-                                   parent=self.parent,
-                                   root=self.root)
-            self.right_sibling.left_sibling = new_tree
-            self.right_sibling = new_tree
-        return self.right_sibling
-
-    # TODO: implement these methods:
-    def delete_node(self):
-        return
-
-    def delete_leftchild(self):
-        return
-
-    def delete_rightsibling(self):
-        return
-
-    def delete_subtree(self):
-        return
+"""
+The following functions and variables are for mapping keypresses to the functions that need to be called
+to handle them.
+"""
+# This dictionary maps LaTeX commands to the number of arguments each one takes, and a special instruction,
+# and the character to use to surround the first argument of the Latex command 
+latex_commands = {
+    '/': (2, 'active data'),
+    '^': (1, '', '{}'),
+    '_': (1, '', '{}'),
+    'sqrt': (2, '', '[]'),
+    'sin': (0, '', '{}'),
+    'cos': (0, '', '{}'),
+    'tan': (0, '', '{}'),
+    'cot': (0, '', '{}'),
+    'sec': (0, '', '{}'),
+    'csc': (0, '', '{}'),
+    'arcsin': (0, '', '{}'),
+    'arccos': (0, '', '{}'),
+    'arctan': (0, '', '{}'),
+    'log': (2, '', '_'),
+    'ln': (1, '', '{}'),
+    'lg': (1, '', '{}')
+}
     
-    def walk_tree(self):
-        # Walk left_child's until there are no more, extending the list along the way,
-        # then walk right siblings, then parent's right siblings
+def insert_latex_command(expr, newkey):
+    (num_args, special, arg_char) = latex_command_arguments[newkey.keysym]
+    new_item = LatexCommand(cmd=newkey.keysym, args=num_args, first_arg_char=arg_char)
+    if special == 'active data':
+        new_item.arguments[0] = expr.active_item.data
+    return new_item
 
-        # It is VERY important here to make result a COPY of self.data, rather than a reference to
-        # it.  Otherwise, the list self.data gets mutated by this method.
-        result = self.data[:]   # copy, not reference
-        if self.left_child:
-            result.extend(self.left_child.walk_tree())
-        elif self.right_sibling:
-            result.extend(self.right_sibling.walk_tree())
-        elif self.parent == None:
-            return result
-        elif self.parent.right_sibling != None:
-            result.extend(self.parent.right_sibling.walk_tree())
-        return result
+def erase_cursor(expr):
+    return
+
+def insert_char(expr, newkey):
+    expr.add_string(newkey.keysym)
+    return
+
+def insert_operator(expr, newkey):
+    return
+
+def open_parens(expr, newkey):
+    return
+
+def close_parens(expr, newkey):
+    return
+
+def insert_superscript(expr, newkey):
+    new_item = LatexCommand(cmd='^', args=1)
+    return 
+
+def insert_subscript(expr, newkey):
+    return
+
+def backslash(expr, newkey):
+    return
+    
+BINDINGS = {('/', 0)      : insert_latex_command,
+            ('(', 0)      : open_parens,  
+            (')', 0)      : close_parens,
+            ('^', 0)      : insert_latex_command,
+            ('_', 0)      : insert_latex_command,
+            ('\\', 0)     : backslash,
+            ('plus', 0)   : new_term,
+            ('minus', 0)  : new_term,
+            ('equal', 0)  : new_term,
+            ('plus', 32)  : new_term,
+            ('minus', 32) : new_term,
+            ('equal', 32) : new_term}
+
+for keysym in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890':
+    BINDINGS[(keysym, 0)] = insert_char
+    # 32 is the state for keys on the number pad (at least in OS X - need to check others)
+    BINDINGS[(keysym, 32)] = insert_char
         
-    def count_nodes(self):
-        if self.left_child:
-            return self.left_child.count_nodes() + 1
-        elif self.right_sibling:
-            return self.right_sibling.count_nodes() + 1
-        elif self.parent and self.parent.right_sibling:
-            return self.parent.right_sibling.count_nodes() + 1
-        else:
-            return 1
-
-    def tree_repr(self, string='', indent=''):
-        """
-        return a string representing the tree structure.  Just like print_tree but returns a string
-        instead of printing the result 
-
-        TODO: whiteboard this and get it working right
-        """
-        spaces = '   '
-        s = string + indent + str(self) + '\n'
+def new_binding(keysym, state, new_function):
+    """
+    Could have problems depending on where new_function is defined?
+    """
+    BINDINGS[(keysym, state)] = new_function
         
-        if self.left_child != None:
-            pass
-        elif self.right_sibling != None:
-            pass
-        elif self.parent == None:
-            pass
-        elif self.parent.right_sibling != None:
-            # Since we move up a level, we remove a set of spaces from indent.  Since we can't use 
-            # indent - spaces, we just pass indent with one less 'spaces' indexed:
-            pass
-        else:
-            pass
-        
-    def print_tree(self, indent=''):
-        # How much each level is indented:
-        spaces = '   '
-        print indent + str(self)
-        if self.left_child != None:
-            self.left_child.print_tree(indent=indent+spaces)
-        elif self.right_sibling != None:
-            self.right_sibling.print_tree(indent=indent)
-        elif self.parent == None:
-            return
-        elif self.parent.right_sibling != None:
-            # Since we move up a level, we remove a set of spaces from indent.  Since we can't use 
-            # indent - spaces, we just pass indent with one less 'spaces' indexed:
-            self.parent.right_sibling.print_tree(indent=indent[len(spaces):])
-        else:
-            return
+def get_function_for(keysym, state):
+    # Returns the function that PrettyMath object needs to call to handle the new keypress.
+    # Returns None if the keypress should be ignored
+    # keysym and char are strings; state is an int
+    if (keysym, state) in BINDINGS:
+        return BINDINGS[(keysym, state)]
+    else:
+        # key doesn't have a binding
+        return None
 
-    def __str__(self):
-        return ''.join(self.data)
+class Operator(object):
+    pass
 
+class LatexCommand(object):
+    pass
 
-class PrettyMath(Observable, SiblingTree):
+class PrettyMath(Observable):
     """
     This class has two main attributes: raw, which is a valid Python math expression, and latex
-    which is that expression formatted to look nice in Latex.
+    which is that expression formatted to look nice in Latex.  It's sort of the container or root
+    class for the operators and operands 
     """
-    CURSOR = r'|'
+    CURSOR = '|'
 
-    def __init__(self, 
-                 data=[CURSOR], 
-                 parent=None,
-                 left_child=None,
-                 left_sibling=None,
-                 right_sibling=None,
-                 root=None):
-
-        # Not sure yet how to handle super.init since PrettyMath inherits from two classes.
-        # Need to do a little more research, but for now this works:  
-        Observable.__init__(self)
-        SiblingTree.__init__(self,
-                             data=data,
-                             root=root,
-                             left_child=left_child,
-                             left_sibling=left_sibling,
-                             right_sibling=right_sibling)
-
-        # Put cursor at end of data list
-        # Eventually, the MathEntry widget will draw the cursor on the canvas.  But for now,
-        # we do it really ghetto:
-        # TODO:  Make sure this works right in all cases, especially when starting a new subtree.
-        # I think it will work, but need to spend some time thinking about it.
-        self.cursor_index = len(self.data) - 1
-        # active_node keeps track of which node in the tree is currently being edited/actually contains the cursor
-        self.active_node = self
-        self.running_list = []
-
-    def insert_child(self, new_data):
-        if self.left_child == None:
-            self.left_child = PrettyMath(new_data, root=self.root)
-        else:
-            new_tree = PrettyMath(new_data,
-                                   left_child=self.left_child,
-                                   root=self.root)
-            self.left_child.parent = new_tree
-            self.left_child = new_tree
-        return self.left_child
-
-    # maybe this class definition should have an insert_left_sibling method for the sake of completeness,
-    # even though it won't be used by the PrettyMath class 
-    def insert_rightsibling(self, new_data):
-        if self.right_sibling == None:
-            self.right_sibling = PrettyMath(new_data,
-                                            left_sibling=self,
-                                            parent=self.parent,
-                                            root=self.root)
-        else:
-            new_tree = PrettyMath(new_data,
-                                  right_sibling=self.right_sibling,
-                                  left_sibling=self,
-                                  parent=self.parent,
-                                  root=self.root)
-            self.right_sibling.left_sibling = new_tree
-            self.right_sibling = new_tree
-        return self.right_sibling
-
+    def __init__(self):
+        return self
         
     def __str__(self):
-        return ''.join(self.data)
-
-    def clear_attributes(self):
-        """
-        Set attributes to None so that there are no references to unused, unreachable objects
-        """
-        # TODO: Figure out answer to this question:
-        # Right now this is pretty much identical to __init__. 
-        # Why does it work when calling self.__init__() doesn't?
-        self.left_sibling = None
-        self.right_sibling = None
-        self.parent = None
-        self.left_child = None
-        self.data = [self.CURSOR]
-        self.active_node = self
-        self.cursor_index = 0
-
-    def reset(self):
-        if self.left_child:
-            self.left_child.reset()
-        elif self.right_sibling:
-            self.right_sibling.reset()
-        if self.parent and self.parent.right_sibling:
-            self.parent.right_sibling.reset()
-        
-        self.clear_attributes()
         return
-      
-    def get_latex(self):
-        # Just put '$'s around the string so that matplotlib prints it as LaTeX math
-        return '$' + ''.join(self.walk_tree()) + '$'
-
+    
     def add_keypress(self, newkey):
         # newkey.state is a bitfield that represents modifier keys pressed simultaneously with
         # the keysym.  It is 0 if there are no modifier keys:
-        func = keybindings.get_function_for(newkey.keysym, newkey.char, newkey.state)
+        func = self.binding_map.get_function_for(newkey)
 
         if func == None:
             return
         else:
-            func(self.active_node, newkey)
+            func(self, newkey)
+        
+        # Just a placeholder as a reminder:
+        self.check_for_latex_command('')
 
-        # Check to see if the end part of running_list is something meaningful in LaTeX:
-        command = self.check_for_latex_command(''.join(self.active_node.running_list))
-        if command:
-            size = len(command)
-            # Remove all the individual letters that spell the command
-            del self.active_node.data[ self.cursor_index - size : self.cursor_index ]
-            
-            # Replace them with a single string element containing the LaTeX command
-            self.active_node.data.insert(self.cursor_index - size, LATEX_COMMANDS[command])
-
-            # Reset running_list if it just inserted a function, otherwise make running_list
-            # hold the current term.
-            if command in FUNCTIONS:
-                self.running_list = []
-            else:
-                del self.running_list[-size:]
-                self.running_list.append(LATEX_COMMANDS[command])
-
-            # Make the cursor_index right:
-            self.cursor_index += 1
-            
-        # It's handy to have cursor_index accessible from both the root and the active_node 
-        self.active_node.cursor_index = self.cursor_index
         # This should be the last thing we do:
         self.notify_observers()
-        print 'cursor index =', self.cursor_index
-        self.print_tree()
-        print
 
 
     def check_for_latex_command(self, string):
